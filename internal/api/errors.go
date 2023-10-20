8dc3ec0c906194dead87f6954e87615d34d0c169
Golang Lemondade <147884153+golanglemonade@users.noreply.github.com>
2023-10-20 14:16:11 -0600

    remove unused

--- internal/api/errors.go
+++ internal/api/errors.go
@@ -5,6 +5,4 @@ import "errors"
 var (
 	// ErrInternalServerError is returned when an internal error occurs.
 	ErrInternalServerError = errors.New("internal server error")
-
-	ErrNotFoundError = errors.New("not found")
 )

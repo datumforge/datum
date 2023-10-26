31a5ecab28573e8ae50278795ae072e66df36f44
Matthew Anderson <42154938+matoszz@users.noreply.github.com>
2023-10-26 14:33:02 -0600

    add groups to schema
    Signed-off-by: Matthew Anderson <42154938+matoszz@users.noreply.github.com>

---
+++ internal/ent/schema/group.go
@@ -0,0 +1,47 @@
+package schema
+
+import (
+	"entgo.io/contrib/entgql"
+	"entgo.io/ent"
+	"entgo.io/ent/dialect/entsql"
+	"entgo.io/ent/schema"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+	"github.com/google/uuid"
+)
+
+// Group holds the schema definition for the Group entity.
+type Group struct {
+	ent.Schema
+}
+
+// Fields of the Group.
+func (Group) Fields() []ent.Field {
+	return []ent.Field{
+		field.UUID("id", uuid.UUID{}).Default(uuid.New).Unique(),
+		field.String("name").NotEmpty().Unique(),
+		field.String("description").Default("").Annotations(
+			entgql.Skip(entgql.SkipWhereInput),
+		),
+		field.String("logo_url").NotEmpty().Annotations(
+			entgql.Skip(entgql.SkipWhereInput),
+		),
+	}
+}
+
+// Edges of the Group.
+func (Group) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.To("settings", GroupSettings.Type).Required().Unique(),
+		edge.To("memberships", Membership.Type).Annotations(entsql.Annotation{OnDelete: entsql.Cascade}),
+	}
+}
+
+// Annotations of the Group.
+func (Group) Annotations() []schema.Annotation {
+	return []schema.Annotation{
+		entgql.RelayConnection(),
+		entgql.QueryField(),
+		entgql.Mutations(entgql.MutationCreate(), (entgql.MutationUpdate())),
+	}
+}

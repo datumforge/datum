6a9e0f99372af0c2f42502abe8bc375ad4f25c4b
Golang Lemondade <147884153+golanglemonade@users.noreply.github.com>
2023-11-08 07:51:36 -0700

    add more organization queries and mutations

--- internal/datumclient/client.go
+++ internal/datumclient/client.go
@@ -13,6 +13,9 @@ import (
 type DatumClient interface {
 	GetOrganizationByID(ctx context.Context, organizationID string, interceptors ...clientv2.RequestInterceptor) (*GetOrganizationByID, error)
 	GetAllOrganizations(ctx context.Context, interceptors ...clientv2.RequestInterceptor) (*GetAllOrganizations, error)
+	CreateOrganization(ctx context.Context, input CreateOrganizationInput, interceptors ...clientv2.RequestInterceptor) (*CreateOrganization, error)
+	UpdateOrganization(ctx context.Context, updateOrganizationID string, input UpdateOrganizationInput, interceptors ...clientv2.RequestInterceptor) (*UpdateOrganization, error)
+	DeleteOrganization(ctx context.Context, deleteOrganizationID string, interceptors ...clientv2.RequestInterceptor) (*DeleteOrganization, error)
 }
 
 type Client struct {
@@ -153,6 +156,139 @@ func (t *GetAllOrganizations_Organizations) GetEdges() []*GetAllOrganizations_Or
 	return t.Edges
 }
 
+type CreateOrganization_CreateOrganization_Organization_Parent struct {
+	ID   string "json:\"id\" graphql:\"id\""
+	Name string "json:\"name\" graphql:\"name\""
+}
+
+func (t *CreateOrganization_CreateOrganization_Organization_Parent) GetID() string {
+	if t == nil {
+		t = &CreateOrganization_CreateOrganization_Organization_Parent{}
+	}
+	return t.ID
+}
+func (t *CreateOrganization_CreateOrganization_Organization_Parent) GetName() string {
+	if t == nil {
+		t = &CreateOrganization_CreateOrganization_Organization_Parent{}
+	}
+	return t.Name
+}
+
+type CreateOrganization_CreateOrganization_Organization struct {
+	ID          string                                                     "json:\"id\" graphql:\"id\""
+	Name        string                                                     "json:\"name\" graphql:\"name\""
+	Description *string                                                    "json:\"description,omitempty\" graphql:\"description\""
+	Parent      *CreateOrganization_CreateOrganization_Organization_Parent "json:\"parent,omitempty\" graphql:\"parent\""
+}
+
+func (t *CreateOrganization_CreateOrganization_Organization) GetID() string {
+	if t == nil {
+		t = &CreateOrganization_CreateOrganization_Organization{}
+	}
+	return t.ID
+}
+func (t *CreateOrganization_CreateOrganization_Organization) GetName() string {
+	if t == nil {
+		t = &CreateOrganization_CreateOrganization_Organization{}
+	}
+	return t.Name
+}
+func (t *CreateOrganization_CreateOrganization_Organization) GetDescription() *string {
+	if t == nil {
+		t = &CreateOrganization_CreateOrganization_Organization{}
+	}
+	return t.Description
+}
+func (t *CreateOrganization_CreateOrganization_Organization) GetParent() *CreateOrganization_CreateOrganization_Organization_Parent {
+	if t == nil {
+		t = &CreateOrganization_CreateOrganization_Organization{}
+	}
+	return t.Parent
+}
+
+type CreateOrganization_CreateOrganization struct {
+	Organization CreateOrganization_CreateOrganization_Organization "json:\"organization\" graphql:\"organization\""
+}
+
+func (t *CreateOrganization_CreateOrganization) GetOrganization() *CreateOrganization_CreateOrganization_Organization {
+	if t == nil {
+		t = &CreateOrganization_CreateOrganization{}
+	}
+	return &t.Organization
+}
+
+type UpdateOrganization_UpdateOrganization_Organization_Parent struct {
+	ID   string "json:\"id\" graphql:\"id\""
+	Name string "json:\"name\" graphql:\"name\""
+}
+
+func (t *UpdateOrganization_UpdateOrganization_Organization_Parent) GetID() string {
+	if t == nil {
+		t = &UpdateOrganization_UpdateOrganization_Organization_Parent{}
+	}
+	return t.ID
+}
+func (t *UpdateOrganization_UpdateOrganization_Organization_Parent) GetName() string {
+	if t == nil {
+		t = &UpdateOrganization_UpdateOrganization_Organization_Parent{}
+	}
+	return t.Name
+}
+
+type UpdateOrganization_UpdateOrganization_Organization struct {
+	ID          string                                                     "json:\"id\" graphql:\"id\""
+	Name        string                                                     "json:\"name\" graphql:\"name\""
+	Parent      *UpdateOrganization_UpdateOrganization_Organization_Parent "json:\"parent,omitempty\" graphql:\"parent\""
+	Description *string                                                    "json:\"description,omitempty\" graphql:\"description\""
+}
+
+func (t *UpdateOrganization_UpdateOrganization_Organization) GetID() string {
+	if t == nil {
+		t = &UpdateOrganization_UpdateOrganization_Organization{}
+	}
+	return t.ID
+}
+func (t *UpdateOrganization_UpdateOrganization_Organization) GetName() string {
+	if t == nil {
+		t = &UpdateOrganization_UpdateOrganization_Organization{}
+	}
+	return t.Name
+}
+func (t *UpdateOrganization_UpdateOrganization_Organization) GetParent() *UpdateOrganization_UpdateOrganization_Organization_Parent {
+	if t == nil {
+		t = &UpdateOrganization_UpdateOrganization_Organization{}
+	}
+	return t.Parent
+}
+func (t *UpdateOrganization_UpdateOrganization_Organization) GetDescription() *string {
+	if t == nil {
+		t = &UpdateOrganization_UpdateOrganization_Organization{}
+	}
+	return t.Description
+}
+
+type UpdateOrganization_UpdateOrganization struct {
+	Organization UpdateOrganization_UpdateOrganization_Organization "json:\"organization\" graphql:\"organization\""
+}
+
+func (t *UpdateOrganization_UpdateOrganization) GetOrganization() *UpdateOrganization_UpdateOrganization_Organization {
+	if t == nil {
+		t = &UpdateOrganization_UpdateOrganization{}
+	}
+	return &t.Organization
+}
+
+type DeleteOrganization_DeleteOrganization struct {
+	DeletedID string "json:\"deletedID\" graphql:\"deletedID\""
+}
+
+func (t *DeleteOrganization_DeleteOrganization) GetDeletedID() string {
+	if t == nil {
+		t = &DeleteOrganization_DeleteOrganization{}
+	}
+	return t.DeletedID
+}
+
 type GetOrganizationByID struct {
 	Organization GetOrganizationByID_Organization "json:\"organization\" graphql:\"organization\""
 }
@@ -173,6 +309,39 @@ func (t *GetAllOrganizations) GetOrganizations() *GetAllOrganizations_Organizati
 		t = &GetAllOrganizations{}
 	}
 	return &t.Organizations
+}
+
+type CreateOrganization struct {
+	CreateOrganization CreateOrganization_CreateOrganization "json:\"createOrganization\" graphql:\"createOrganization\""
+}
+
+func (t *CreateOrganization) GetCreateOrganization() *CreateOrganization_CreateOrganization {
+	if t == nil {
+		t = &CreateOrganization{}
+	}
+	return &t.CreateOrganization
+}
+
+type UpdateOrganization struct {
+	UpdateOrganization UpdateOrganization_UpdateOrganization "json:\"updateOrganization\" graphql:\"updateOrganization\""
+}
+
+func (t *UpdateOrganization) GetUpdateOrganization() *UpdateOrganization_UpdateOrganization {
+	if t == nil {
+		t = &UpdateOrganization{}
+	}
+	return &t.UpdateOrganization
+}
+
+type DeleteOrganization struct {
+	DeleteOrganization DeleteOrganization_DeleteOrganization "json:\"deleteOrganization\" graphql:\"deleteOrganization\""
+}
+
+func (t *DeleteOrganization) GetDeleteOrganization() *DeleteOrganization_DeleteOrganization {
+	if t == nil {
+		t = &DeleteOrganization{}
+	}
+	return &t.DeleteOrganization
 }
 
 const GetOrganizationByIDDocument = `query GetOrganizationByID ($organizationId: ID!) {
@@ -223,6 +392,95 @@ func (c *Client) GetAllOrganizations(ctx context.Context, interceptors ...client
 
 	var res GetAllOrganizations
 	if err := c.Client.Post(ctx, "GetAllOrganizations", GetAllOrganizationsDocument, &res, vars, interceptors...); err != nil {
+		if c.Client.ParseDataWhenErrors {
+			return &res, err
+		}
+
+		return nil, err
+	}
+
+	return &res, nil
+}
+
+const CreateOrganizationDocument = `mutation CreateOrganization ($input: CreateOrganizationInput!) {
+	createOrganization(input: $input) {
+		organization {
+			id
+			name
+			description
+			parent {
+				id
+				name
+			}
+		}
+	}
+}
+`
+
+func (c *Client) CreateOrganization(ctx context.Context, input CreateOrganizationInput, interceptors ...clientv2.RequestInterceptor) (*CreateOrganization, error) {
+	vars := map[string]interface{}{
+		"input": input,
+	}
+
+	var res CreateOrganization
+	if err := c.Client.Post(ctx, "CreateOrganization", CreateOrganizationDocument, &res, vars, interceptors...); err != nil {
+		if c.Client.ParseDataWhenErrors {
+			return &res, err
+		}
+
+		return nil, err
+	}
+
+	return &res, nil
+}
+
+const UpdateOrganizationDocument = `mutation UpdateOrganization ($updateOrganizationId: ID!, $input: UpdateOrganizationInput!) {
+	updateOrganization(id: $updateOrganizationId, input: $input) {
+		organization {
+			id
+			name
+			parent {
+				id
+				name
+			}
+			description
+		}
+	}
+}
+`
+
+func (c *Client) UpdateOrganization(ctx context.Context, updateOrganizationID string, input UpdateOrganizationInput, interceptors ...clientv2.RequestInterceptor) (*UpdateOrganization, error) {
+	vars := map[string]interface{}{
+		"updateOrganizationId": updateOrganizationID,
+		"input":                input,
+	}
+
+	var res UpdateOrganization
+	if err := c.Client.Post(ctx, "UpdateOrganization", UpdateOrganizationDocument, &res, vars, interceptors...); err != nil {
+		if c.Client.ParseDataWhenErrors {
+			return &res, err
+		}
+
+		return nil, err
+	}
+
+	return &res, nil
+}
+
+const DeleteOrganizationDocument = `mutation DeleteOrganization ($deleteOrganizationId: ID!) {
+	deleteOrganization(id: $deleteOrganizationId) {
+		deletedID
+	}
+}
+`
+
+func (c *Client) DeleteOrganization(ctx context.Context, deleteOrganizationID string, interceptors ...clientv2.RequestInterceptor) (*DeleteOrganization, error) {
+	vars := map[string]interface{}{
+		"deleteOrganizationId": deleteOrganizationID,
+	}
+
+	var res DeleteOrganization
+	if err := c.Client.Post(ctx, "DeleteOrganization", DeleteOrganizationDocument, &res, vars, interceptors...); err != nil {
 		if c.Client.ParseDataWhenErrors {
 			return &res, err
 		}

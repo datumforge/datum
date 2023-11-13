5701c06fd9f44ff3bccfbfc4cbc39d71a7624ba9
Golang Lemondade <147884153+golanglemonade@users.noreply.github.com>
2023-11-13 13:35:21 -0700

    comment
    Signed-off-by: Golang Lemondade <147884153+golanglemonade@users.noreply.github.com>

--- cmd/cli/cmd/org.go
+++ cmd/cli/cmd/org.go
@@ -2,7 +2,7 @@ package datum
 
 import "github.com/spf13/cobra"
 
-// orgCmd represents the base report command when called without any subcommands
+// orgCmd represents the base org command when called without any subcommands
 var orgCmd = &cobra.Command{
 	Use:   "org",
 	Short: "The subcommands for working with the datum organiation",

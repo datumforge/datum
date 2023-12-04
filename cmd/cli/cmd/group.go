258bb561c4b20c842b480fa135afe42282b83c39
Matt Anderson <42154938+matoszz@users.noreply.github.com>
2023-12-04 12:15:08 -0600

    Update cmd/cli/cmd/group.go
    Co-authored-by: golanglemonade <147884153+golanglemonade@users.noreply.github.com>
    Signed-off-by: Matt Anderson <42154938+matoszz@users.noreply.github.com>

--- cmd/cli/cmd/group.go
+++ cmd/cli/cmd/group.go
@@ -2,7 +2,7 @@ package datum
 
 import "github.com/spf13/cobra"
 
-// orgCmd represents the base org command when called without any subcommands
+// groupCmd represents the base group command when called without any subcommands
 var groupCmd = &cobra.Command{
 	Use:   "group",
 	Short: "The subcommands for working with datum groups",

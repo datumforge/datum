9da1f40377a6933e9083809734caf62c0b2b0d97
Golang Lemonade <147884153+golanglemonade@users.noreply.github.com>
2023-12-11 09:24:08 -0700

    those are not needed
    Signed-off-by: Golang Lemonade <147884153+golanglemonade@users.noreply.github.com>

--- internal/ent/mixin/idmixin.go
+++ internal/ent/mixin/idmixin.go
@@ -1,13 +1,9 @@
 package mixin
 
 import (
-	"entgo.io/contrib/entgql"
-	"entgo.io/contrib/entoas"
 	"entgo.io/ent"
 	"entgo.io/ent/schema/field"
 	"entgo.io/ent/schema/mixin"
-
-	"github.com/ogen-go/ogen"
 
 	"github.com/datumforge/datum/internal/utils/ulids"
 )
@@ -22,9 +18,6 @@ func (IDMixin) Fields() []ent.Field {
 	return []ent.Field{
 		field.String("id").
 			Immutable().
-			DefaultFunc(func() string { return ulids.New().String() }).
-			Annotations(
-				entgql.Type("ID"),
-				entoas.Schema(ogen.String())),
+			DefaultFunc(func() string { return ulids.New().String() }),
 	}
 }

4b5032ec189dce8a8b6c08b87b18a22826e7044e
Golang Lemonade <147884153+golanglemonade@users.noreply.github.com>
2023-12-14 13:56:49 -0700

    squash db migrations
    Signed-off-by: Golang Lemonade <147884153+golanglemonade@users.noreply.github.com>

--- db/migrations/20231214205613_display_name.sql
+++ db/migrations/20231214205613_display_name.sql
@@ -10,6 +10,16 @@ DROP TABLE `organizations`;
 ALTER TABLE `new_organizations` RENAME TO `organizations`;
 -- Create index "organization_name" to table: "organizations"
 CREATE UNIQUE INDEX `organization_name` ON `organizations` (`name`) WHERE deleted_at is NULL;
+-- Create "new_groups" table
+CREATE TABLE `new_groups` (`id` text NOT NULL, `created_at` datetime NOT NULL, `updated_at` datetime NOT NULL, `created_by` text NULL, `updated_by` text NULL, `deleted_at` datetime NULL, `deleted_by` text NULL, `name` text NOT NULL, `description` text NOT NULL DEFAULT (''), `logo_url` text NOT NULL, `display_name` text NOT NULL DEFAULT (''), `organization_groups` text NOT NULL, PRIMARY KEY (`id`), CONSTRAINT `groups_organizations_groups` FOREIGN KEY (`organization_groups`) REFERENCES `organizations` (`id`) ON DELETE CASCADE);
+-- Copy rows from old table "groups" to new temporary table "new_groups"
+INSERT INTO `new_groups` (`id`, `created_at`, `updated_at`, `created_by`, `updated_by`, `deleted_at`, `deleted_by`, `name`, `description`, `logo_url`, `display_name`, `organization_groups`) SELECT `id`, `created_at`, `updated_at`, `created_by`, `updated_by`, `deleted_at`, `deleted_by`, `name`, `description`, `logo_url`, IFNULL(`display_name`, ('')) AS `display_name`, `organization_groups` FROM `groups`;
+-- Drop "groups" table after copying rows
+DROP TABLE `groups`;
+-- Rename temporary table "new_groups" to "groups"
+ALTER TABLE `new_groups` RENAME TO `groups`;
+-- Create index "group_name_organization_groups" to table: "groups"
+CREATE UNIQUE INDEX `group_name_organization_groups` ON `groups` (`name`, `organization_groups`) WHERE deleted_at is NULL;
 -- Create "new_users" table
 CREATE TABLE `new_users` (`id` text NOT NULL, `created_at` datetime NOT NULL, `updated_at` datetime NOT NULL, `created_by` text NULL, `updated_by` text NULL, `deleted_at` datetime NULL, `deleted_by` text NULL, `email` text NOT NULL, `first_name` text NOT NULL, `last_name` text NOT NULL, `display_name` text NOT NULL DEFAULT (''), `avatar_remote_url` text NULL, `avatar_local_file` text NULL, `avatar_updated_at` datetime NULL, `last_seen` datetime NULL, `password` text NULL, `sub` text NULL, `oauth` bool NOT NULL DEFAULT (false), PRIMARY KEY (`id`));
 -- Copy rows from old table "users" to new temporary table "new_users"

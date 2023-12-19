6af2f51db570f336ced40cace8c3179c1c647b97
Matthew Anderson <42154938+matoszz@users.noreply.github.com>
2023-12-18 20:13:57 -0700

    updated task file

--- scripts/gitdiff.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -ueo pipefail
-
-status=$(git status --porcelain)
-if [ -n "$status" ]; then
-    echo "detected git diff after running generate; please re-run tasks"
-    echo "$status"
-    exit 1
-fi

503a8cc2cca15065fa45c07f1aa58d542c5d22bd
Matthew Anderson <42154938+matoszz@users.noreply.github.com>
2023-12-19 21:54:34 -0700

    gofmt

--- internal/ent/hooks/session.go
+++ internal/ent/hooks/session.go
@@ -14,10 +14,10 @@ import (
 func HookSession() ent.Hook {
 	return hook.On(func(next ent.Mutator) ent.Mutator {
 		return hook.SessionFunc(func(ctx context.Context, mutation *generated.SessionMutation) (generated.Value, error) {
-		        expires, _ := mutation.ExpiresAt()
-		        if expires.IsZero() {
-			        mutation.SetExpiresAt(time.Now().Add(time.Hour * 24 * 7)) // nolint: gomnd
-		        }
+			expires, _ := mutation.ExpiresAt()
+			if expires.IsZero() {
+				mutation.SetExpiresAt(time.Now().Add(time.Hour * 24 * 7)) // nolint: gomnd
+			}
 
 			return next.Mutate(ctx, mutation)
 		})

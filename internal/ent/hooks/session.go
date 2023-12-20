832a613b4c974f0cbf47070dc3034118d7ba3b5e
Matt Anderson <42154938+matoszz@users.noreply.github.com>
2023-12-19 21:39:22 -0700

    Update internal/ent/hooks/session.go
    Co-authored-by: golanglemonade <147884153+golanglemonade@users.noreply.github.com>
    Signed-off-by: Matt Anderson <42154938+matoszz@users.noreply.github.com>

--- internal/ent/hooks/session.go
+++ internal/ent/hooks/session.go
@@ -14,14 +14,12 @@ import (
 func HookSession() ent.Hook {
 	return hook.On(func(next ent.Mutator) ent.Mutator {
 		return hook.SessionFunc(func(ctx context.Context, mutation *generated.SessionMutation) (generated.Value, error) {
-			if mutation.Op().Is(ent.OpCreate) {
-				expires, _ := mutation.ExpiresAt()
-				if expires.IsZero() {
-					mutation.SetExpiresAt(time.Now().Add(time.Hour * 24 * 7)) // nolint: gomnd
-				}
-			}
+		        expires, _ := mutation.ExpiresAt()
+		        if expires.IsZero() {
+			        mutation.SetExpiresAt(time.Now().Add(time.Hour * 24 * 7)) // nolint: gomnd
+		        }
 
 			return next.Mutate(ctx, mutation)
 		})
-	}, ent.OpCreate|ent.OpUpdateOne)
+	}, ent.OpCreate)
 }

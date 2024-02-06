1226ff751832b1d1a8a1d4d88ea44d605741170b
Golang Lemonade <147884153+golanglemonade@users.noreply.github.com>
2024-02-06 10:59:23 -0700

    pr feedback: comments
    Signed-off-by: Golang Lemonade <147884153+golanglemonade@users.noreply.github.com>

--- internal/tokens/exchange.go
+++ internal/tokens/exchange.go
@@ -1,5 +1,7 @@
 package tokens
 
+// ExchangeTokenRequest is the request response when exchanging an oauth token from one provider
+// to a Datum issued token
 type ExchangeTokenResponse struct {
 	AccessToken string `json:"access_token"`
 	TokenType   string `json:"token_type"`
--- internal/tokens/oidc.go
+++ internal/tokens/oidc.go
@@ -1,5 +1,10 @@
 package tokens
 
+// DiscoveryJSON is the response from the OIDC discovery endpoint and contains supported scopes and claims,
+// public keys used to sign the tokens, issuer, and other information.
+// Clients can use this information to construct a request to the OpenID server.
+// The field names and values are defined in the OpenID Connect Discovery Specification
+// https://openid.net/specs/openid-connect-discovery-1_0.html
 type DiscoveryJSON struct {
 	Issuer                            string   `json:"issuer"`
 	AuthorizationEndpoint             string   `json:"authorization_endpoint"`

7a1a9783a61fa8592f8074c5881b9b3377e0fa76
Matthew Anderson <42154938+matoszz@users.noreply.github.com>
2024-02-06 19:52:27 -0700

    live all dangerous like sarah funkhouser

--- db/backup/feb_2024/20231130080026_add-soft-delete.sql
+++
@@ -1,4 +0,0 @@
--- Add column "deleted_at" to table: "organizations"
-ALTER TABLE `organizations` ADD COLUMN `deleted_at` datetime NULL;
--- Add column "deleted_by" to table: "organizations"
-ALTER TABLE `organizations` ADD COLUMN `deleted_by` text NULL;

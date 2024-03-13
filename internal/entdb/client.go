0f52b01b2c4e4ad782acc250a50aa2fab9c2a27e
Golang Lemonade <147884153+golanglemonade@users.noreply.github.com>
2024-03-12 20:20:22 -0600

    terminate container
    Signed-off-by: Golang Lemonade <147884153+golanglemonade@users.noreply.github.com>

--- internal/entdb/client.go
+++ internal/entdb/client.go
@@ -2,6 +2,7 @@ package entdb
 
 import (
 	"context"
+	"log"
 	"os"
 	"time"
 
@@ -158,6 +159,13 @@ func NewTestClient(ctx context.Context, entOpts []ent.Option) (*ent.Client, *tes
 	if err := db.Schema.Create(ctx); err != nil {
 		return nil, nil, err
 	}
+
+	// Clean up the container
+	defer func() {
+		if err := ctr.Container.Terminate(ctx); err != nil {
+			log.Fatalf("failed to terminate container: %s", err)
+		}
+	}()
 
 	return db, ctr, nil
 }

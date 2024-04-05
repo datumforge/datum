44ec4bbec8e9928291b1cb5b3ebac8d59feafb09
Golang Lemonade <147884153+golanglemonade@users.noreply.github.com>
2024-04-05 11:53:04 -0600

    add cli
    Signed-off-by: Golang Lemonade <147884153+golanglemonade@users.noreply.github.com>

---
+++ cmd/cli/cmd/subscribers/subscriber_create.go
@@ -0,0 +1,63 @@
+package datumsubscribers
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	datum "github.com/datumforge/datum/cmd/cli/cmd"
+	"github.com/datumforge/datum/internal/datumclient"
+)
+
+var subscribersCreateCmd = &cobra.Command{
+	Use:   "create",
+	Short: "Add subscribers to a datum org",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return subscriberCreate(cmd.Context())
+	},
+}
+
+func init() {
+	subscribersCmd.AddCommand(subscribersCreateCmd)
+
+	subscribersCreateCmd.Flags().StringP("email", "e", "", "email address of the subscriber")
+	datum.ViperBindFlag("subscribers.create.email", subscribersCreateCmd.Flags().Lookup("email"))
+}
+
+func subscriberCreate(ctx context.Context) error {
+	// setup datum http client
+	cli, err := datum.GetGraphClient(ctx)
+	if err != nil {
+		return err
+	}
+
+	// save session cookies on function exit
+	client, _ := cli.Client.(*datumclient.Client)
+	defer datum.StoreSessionCookies(client)
+
+	email := viper.GetString("subscribers.create.email")
+	if email == "" {
+		return datum.NewRequiredFieldMissingError("email")
+	}
+
+	// filter options
+	input := datumclient.CreateSubscriberInput{
+		Email: email,
+	}
+
+	var s []byte
+
+	sub, err := cli.Client.CreateSubscriber(ctx, input, cli.Interceptor)
+	if err != nil {
+		return err
+	}
+
+	s, err = json.Marshal(sub)
+	if err != nil {
+		return err
+	}
+
+	return datum.JSONPrint(s)
+}

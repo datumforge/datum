55bca59f69fc993600dafe866dc8df30fda0f0bd
Matthew Anderson <42154938+matoszz@users.noreply.github.com>
2024-04-10 11:25:32 -0600

    fix flakey tests

--- pkg/utils/storage/fs/fs_test.go
+++ pkg/utils/storage/fs/fs_test.go
@@ -8,6 +8,8 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
+
 	gostorage "github.com/datumforge/datum/pkg/utils/storage"
 )
 
@@ -41,11 +43,5 @@ func Test(t *testing.T) {
 		t.Errorf("expected size to be %d, got %d", 5, stat.Size)
 	}
 
-	if stat.ModifiedTime.Before(before) {
-		t.Errorf("expected modtime to be after %v, got %v", before, stat.ModifiedTime)
-	}
-
-	if stat.ModifiedTime.After(now) {
-		t.Errorf("expected modtime to be before %v, got %v", now, stat.ModifiedTime)
-	}
+	assert.WithinDuration(t, before, now, time.Second)
 }

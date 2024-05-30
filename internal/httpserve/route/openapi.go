b0c0703976cb49a9370b1deb0e14c3794f2358e1
Matthew Anderson <42154938+matoszz@users.noreply.github.com>
2024-05-30 12:33:06 -0400

    remove unused openapi functions

--- internal/httpserve/route/openapi.go
+++ internal/httpserve/route/openapi.go
@@ -1,12 +1,8 @@
 package route
 
 import (
-	"fmt"
-	"reflect"
-
 	echo "github.com/datumforge/echox"
 	"github.com/getkin/kin-openapi/openapi3"
-	"github.com/getkin/kin-openapi/openapi3gen"
 
 	"github.com/datumforge/datum/internal/httpserve/handlers"
 )
@@ -30,6 +26,7 @@ func (r *Router) AddRoute(pattern, method string, op *openapi3.Operation, route 
 	return nil
 }
 
+// AddEchoOnlyRoute is used to add a route to the echo router without adding it to the OpenAPI schema
 func (r *Router) AddEchoOnlyRoute(pattern, method string, route echo.Routable) error {
 	_, err := r.Echo.AddRoute(route)
 	if err != nil {
@@ -37,98 +34,4 @@ func (r *Router) AddEchoOnlyRoute(pattern, method string, route echo.Routable) e
 	}
 
 	return nil
-}
-
-// OpenAPI returns the OpenAPI specification.
-func (r *Router) OpenAPI() *openapi3.T {
-	return r.OAS
-}
-
-// AddQueryParameter adds a query parameter to the OpenAPI schema
-func (r *Router) AddQueryParameter(name string, value Parameter) {
-	checkTags(reflect.TypeOf(value))
-
-	param := &openapi3.Parameter{
-		Name:        value.Name,
-		In:          value.In,
-		Description: value.Description,
-		Required:    value.Required,
-		Schema: &openapi3.SchemaRef{
-			Value: &openapi3.Schema{
-				Default: value.Default,
-				Type:    (*openapi3.Types)(&value.Types),
-			},
-		},
-	}
-	r.OAS.Components.Parameters[name] = &openapi3.ParameterRef{Value: param}
-}
-
-// Parameter is a struct that represents a parameter in the OpenAPI schema
-type Parameter struct {
-	Name        string      `json:"name" yaml:"name"`
-	Description string      `json:"description" yaml:"description"`
-	Required    bool        `json:"required" yaml:"required"`
-	Default     interface{} `json:"default" yaml:"default"`
-	Types       []string    `json:"type" yaml:"type"`
-	In          string      `json:"in" yaml:"in"`
-}
-
-// TokenQuery is a parameter for the token query
-var TokenQuery = Parameter{
-	Name:        "token",
-	Description: "the token to parse out of a URL",
-	Types:       []string{"integer"},
-	Required:    true,
-	In:          "query",
-}
-
-// checkTags is a helper function that checks if a struct has the required JSON or YAML tags
-func checkTags(rval reflect.Type) {
-	if rval.Kind() == reflect.Array || rval.Kind() == reflect.Slice {
-		checkTags(rval.Elem())
-		return
-	}
-
-	if rval.Kind() != reflect.Struct {
-		fmt.Printf("unable to check type %s of kind %s for struct tags, skipped\n", rval.Name(), rval.Kind().String())
-		return
-	}
-
-	for i := 0; i < rval.NumField(); i++ {
-		for _, tagName := range []string{"json", "yaml"} {
-			if _, ok := rval.Field(i).Tag.Lookup(tagName); !ok {
-				return
-			}
-		}
-	}
-}
-
-// AddSchema adds a schema to the OpenAPI schema
-func (r *Router) AddSchema(name string, model interface{}) error {
-	schema, err := openapi3gen.NewSchemaRefForValue(model, r.OAS.Components.Schemas)
-	if err != nil {
-		return err
-	}
-
-	r.OAS.Components.Schemas[name] = schema
-
-	return nil
-}
-
-// SchemaGenerator is a helper function that generates a schema from a map
-// example: var openapiSchemas = map[string]any{LoginRequest: LoginRequest{}}
-func SchemaGenerator(openAPISchemas map[string]any) (openapi3.Schemas, error) {
-	schemas := make(openapi3.Schemas)
-	generator := openapi3gen.NewGenerator(openapi3gen.UseAllExportedFields())
-
-	for key, val := range openAPISchemas {
-		ref, err := generator.NewSchemaRefForValue(val, schemas)
-		if err != nil {
-			return nil, err
-		}
-
-		schemas[key] = ref
-	}
-
-	return schemas, nil
 }

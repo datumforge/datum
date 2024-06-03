58c3d3e23887e917771df182d0e0c6041b9bc53f
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2024-06-03 14:48:27 -0600

    use docker test instead of docker compose for migrations container
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- db/create_migrations.go
+++ db/create_migrations.go
@@ -4,12 +4,14 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"log"
 	"os"
 
 	// supported ent database drivers
-	_ "github.com/datumforge/entx"                       // overlay for sqlite
-	_ "github.com/lib/pq"                                // postgres driver
+	_ "github.com/datumforge/entx" // overlay for sqlite
+	_ "github.com/lib/pq"          // postgres driver
+	"github.com/ory/dockertest"
 	_ "github.com/tursodatabase/libsql-client-go/libsql" // libsql driver
 	_ "modernc.org/sqlite"                               // sqlite driver (non-cgo)
 
@@ -19,6 +21,7 @@ import (
 	atlas "ariga.io/atlas/sql/migrate"
 	"ariga.io/atlas/sql/sqltool"
 	"github.com/datumforge/datum/internal/ent/generated/migrate"
+	"github.com/datumforge/datum/pkg/testutils"
 )
 
 func main() {
@@ -59,10 +62,8 @@ func main() {
 		log.Fatalln("failed to load the ATLAS_SQLITE_DB_URI env var")
 	}
 
-	pgDBURI, ok := os.LookupEnv("ATLAS_POSTGRES_DB_URI")
-	if !ok {
-		log.Fatalln("failed to load the ATLAS_POSTGRES_DB_URI env var")
-	}
+	tf := createPostgresTestContainer()
+	defer testutils.TeardownFixture(tf)
 
 	// Generate migrations using Atlas support for sqlite (note the Ent dialect option passed above).
 	atlasOpts := append(baseOpts,
@@ -71,7 +72,7 @@ func main() {
 		schema.WithFormatter(atlas.DefaultFormatter),
 	)
 
-	if err := migrate.NamedDiff(ctx, pgDBURI, os.Args[1], atlasOpts...); err != nil {
+	if err := migrate.NamedDiff(ctx, tf.URI, os.Args[1], atlasOpts...); err != nil {
 		log.Fatalf("failed generating atlas migration file: %v", err)
 	}
 
@@ -85,7 +86,42 @@ func main() {
 	// Generate migrations using Goose support for postgres
 	gooseOptsPG := append(postgresOpts, schema.WithDir(gooseDirPG))
 
-	if err = migrate.NamedDiff(ctx, pgDBURI, os.Args[1], gooseOptsPG...); err != nil {
+	if err = migrate.NamedDiff(ctx, tf.URI, os.Args[1], gooseOptsPG...); err != nil {
 		log.Fatalf("failed generating goose migration file for postgres: %v", err)
 	}
+}
+
+// createPostgresTestContainer creates a test postgres container and waits for it to be ready to accept connections
+func createPostgresTestContainer() *testutils.TestFixture {
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("could not construct pool: %s", err)
+	}
+
+	// uses pool to try to connect to Docker
+	err = pool.Client.Ping()
+	if err != nil {
+		log.Fatalf("could not connect to docker: %s", err)
+	}
+
+	pgDBURI, ok := os.LookupEnv("ATLAS_POSTGRES_DB_URI")
+	if !ok {
+		log.Fatalln("failed to load the ATLAS_POSTGRES_DB_URI env var")
+	}
+
+	// create a test postgres container
+	tf := testutils.GetTestURI(pgDBURI, 5) // allow the container to live for 5 minutes
+
+	// exponential backoff-retry, because the application in the container might not be ready to accept connections yet
+	if err := pool.Retry(func() error {
+		db, err := sql.Open("postgres", tf.URI)
+		if err != nil {
+			return err
+		}
+		return db.Ping()
+	}); err != nil {
+		log.Fatalf("Could not connect to database: %s", err)
+	}
+
+	return tf
 }

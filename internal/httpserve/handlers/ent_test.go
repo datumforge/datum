2d3f509a007be6a3b9ea2c8d8d7d54544a5eb354
Samantha Levine <170347878+slevine12@users.noreply.github.com>
2024-07-01 13:01:28 -0400

    refetch google/github/social provider avatars every login

--- internal/httpserve/handlers/ent_test.go
+++ internal/httpserve/handlers/ent_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/brianvoe/gofakeit/v7"
 	"github.com/samber/lo"
 	"github.com/stretchr/testify/assert"
 
@@ -16,6 +17,7 @@ func TestCreateUserInput(t *testing.T) {
 	email := "ww@datum.net"
 	firstName := "Walter"
 	lastName := "White"
+	image := gofakeit.URL()
 
 	testCases := []struct {
 		testName string
@@ -23,6 +25,7 @@ func TestCreateUserInput(t *testing.T) {
 		email    string
 		provider enums.AuthProvider
 		expected ent.CreateUserInput
+		image    string
 	}{
 		{
 			testName: "oauth provider - github",
@@ -30,12 +33,14 @@ func TestCreateUserInput(t *testing.T) {
 			email:    email,
 			provider: enums.AuthProviderGitHub,
 			expected: ent.CreateUserInput{
-				FirstName:    &firstName,
-				LastName:     &lastName,
-				Email:        email,
-				AuthProvider: &enums.AuthProviderGitHub,
-				LastSeen:     lo.ToPtr(time.Now().UTC()),
+				FirstName:       &firstName,
+				LastName:        &lastName,
+				Email:           email,
+				AuthProvider:    &enums.AuthProviderGitHub,
+				LastSeen:        lo.ToPtr(time.Now().UTC()),
+				AvatarRemoteURL: &image,
 			},
+			image: image,
 		},
 		{
 			testName: "oauth provider - github, only first name",
@@ -43,11 +48,13 @@ func TestCreateUserInput(t *testing.T) {
 			email:    email,
 			provider: enums.AuthProviderGitHub,
 			expected: ent.CreateUserInput{
-				FirstName:    lo.ToPtr("meow"),
-				Email:        email,
-				AuthProvider: &enums.AuthProviderGitHub,
-				LastSeen:     lo.ToPtr(time.Now().UTC()),
+				FirstName:       lo.ToPtr("meow"),
+				Email:           email,
+				AuthProvider:    &enums.AuthProviderGitHub,
+				LastSeen:        lo.ToPtr(time.Now().UTC()),
+				AvatarRemoteURL: &image,
 			},
+			image: image,
 		},
 		{
 			testName: "oauth provider - google",
@@ -55,12 +62,14 @@ func TestCreateUserInput(t *testing.T) {
 			email:    email,
 			provider: enums.AuthProviderGoogle,
 			expected: ent.CreateUserInput{
-				FirstName:    &firstName,
-				LastName:     &lastName,
-				Email:        email,
-				AuthProvider: &enums.AuthProviderGoogle,
-				LastSeen:     lo.ToPtr(time.Now().UTC()),
+				FirstName:       &firstName,
+				LastName:        &lastName,
+				Email:           email,
+				AuthProvider:    &enums.AuthProviderGoogle,
+				LastSeen:        lo.ToPtr(time.Now().UTC()),
+				AvatarRemoteURL: &image,
 			},
+			image: image,
 		},
 		{
 			testName: "webauthn provider",
@@ -68,22 +77,40 @@ func TestCreateUserInput(t *testing.T) {
 			email:    email,
 			provider: enums.AuthProviderWebauthn,
 			expected: ent.CreateUserInput{
-				FirstName:    &firstName,
-				LastName:     &lastName,
-				Email:        email,
-				AuthProvider: &enums.AuthProviderWebauthn,
-				LastSeen:     lo.ToPtr(time.Now().UTC()),
+				FirstName:       &firstName,
+				LastName:        &lastName,
+				Email:           email,
+				AuthProvider:    &enums.AuthProviderWebauthn,
+				LastSeen:        lo.ToPtr(time.Now().UTC()),
+				AvatarRemoteURL: &image,
 			},
+			image: image,
+		},
+		{
+			testName: "no image",
+			name:     "Wanda Maximoff",
+			email:    "wmaximoff@marvel.com",
+			provider: enums.AuthProviderWebauthn,
+			expected: ent.CreateUserInput{
+				FirstName:       lo.ToPtr("Wanda"),
+				LastName:        lo.ToPtr("Maximoff"),
+				Email:           "wmaximoff@marvel.com",
+				AuthProvider:    &enums.AuthProviderWebauthn,
+				LastSeen:        lo.ToPtr(time.Now().UTC()),
+				AvatarRemoteURL: nil,
+			},
+			image: "",
 		},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.testName, func(t *testing.T) {
-			input := createUserInput(tc.name, tc.email, tc.provider)
+			input := createUserInput(tc.name, tc.email, tc.provider, tc.image)
 			assert.Equal(t, tc.expected.FirstName, input.FirstName)
 			assert.Equal(t, tc.expected.LastName, input.LastName)
 			assert.Equal(t, tc.expected.Email, input.Email)
 			assert.Equal(t, tc.expected.AuthProvider, input.AuthProvider)
 			assert.WithinDuration(t, *tc.expected.LastSeen, *input.LastSeen, 1*time.Minute) // allow for a reasonable drift while tests are running
+			assert.Equal(t, tc.expected.AvatarRemoteURL, input.AvatarRemoteURL)
 		})
 	}
 }

7f697f044590acb33c767da88bd21ffec10b1d09
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2024-07-19 18:28:00 -0600

    revert rate limit
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- internal/httpserve/route/router.go
+++ internal/httpserve/route/router.go
@@ -16,7 +16,7 @@ var (
 	mw     = []echo.MiddlewareFunc{middleware.Recover()}
 	authMW = []echo.MiddlewareFunc{}
 
-	restrictedRateLimit   = &ratelimit.Config{RateLimit: 10000, BurstLimit: 10, ExpiresIn: 15 * time.Minute} //nolint:mnd
+	restrictedRateLimit   = &ratelimit.Config{RateLimit: 10, BurstLimit: 10, ExpiresIn: 15 * time.Minute} //nolint:mnd
 	restrictedEndpointsMW = []echo.MiddlewareFunc{}
 )
 
